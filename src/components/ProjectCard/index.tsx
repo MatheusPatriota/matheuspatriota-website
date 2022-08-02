7278b8b86561f9dff0682ab573aa249092f85c83
MatheusPatriota <mpatriota55@gmail.com>
2022-08-01 21:48:15 -0300

    feature: projects page done

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,5 +1,58 @@
+import { Box, Button, Link, Text, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { FiExternalLink } from "react-icons/fi";
+interface ProjectTypes {
+  name: string;
+  description: string;
+  url: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
-export default function ProjectCard() {
-  return <div>ProjectCard</div>;
+export default function ProjectCard(props: ProjectTypes) {
+  const { t } = useTranslation();
+  return (
+    <Box
+      display={"flex"}
+      alignItems="center"
+      gap={3}
+      justifyContent={"start"}
+      maxWidth={"300px"}
+      minWidth={"300px"}
+      minHeight={"100px"}
+      maxHeight={"150px"}
+      bg={useColorModeValue("#d2d0d0", "#33303d")}
+      p={3}
+      borderRadius={"10px"}
+    >
+      <Box>
+        <Text fontWeight={"500"} fontSize={"13px"}>
+          {props.name}
+        </Text>
+        <Text fontWeight={"400"} fontSize={"10px"} mt={2}>
+          {props.description}
+        </Text>
+        <Link
+          href={props.url}
+          fontWeight={"100"}
+          fontSize={"12px"}
+          target={"_blank"}
+          textDecoration={"none !Important"}
+        >
+          <Button
+            bg={useColorModeValue("#ffffff", "#000000")}
+            height={"50px"}
+            mt={2}
+            border={useColorModeValue("2px solid #000", "2px solid #fff")}
+            display={"flex"}
+            alignItems={"center"} 
+            gap={2}
+          >
+            {t("source") as string} <FiExternalLink />
+          </Button>
+        </Link>
+      </Box>
+    </Box>
+  );
 }
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,16 +1,28 @@
+import { Box, Text } from "@chakra-ui/react";
 import axios from "axios";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { PageStructure } from "../../components/PageStructure";
+import ProjectCard from "../../components/ProjectCard";
 
+interface ProjectTypes {
+  name: string;
+  description: string;
+  url: string;
+  createdAt: string;
+  updatedAt: string;
+}
 export function Projects() {
+  const [projects, setProjects] = React.useState([]);
+  const { t } = useTranslation();
   const getPinnedRopositories = () => {
     axios("https://api.github.com/graphql", {
       method: "POST",
       headers: {
-        "Authorization": "bearer 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",
-          },
+        Authorization: "bearer " + import.meta.env.VITE_GITHUB_API_KEY,
+      },
       data: JSON.stringify({
-          query: ` {
+        query: ` {
               user(login: "MatheusPatriota") {
                 pinnedItems(first: 6, types: REPOSITORY) {
                   nodes {
@@ -28,7 +40,8 @@ export function Projects() {
       }),
     })
       .then((response) => {
-        console.log(response);
+        console.log(response.data.data.user.pinnedItems.nodes);
+        setProjects(response.data.data.user.pinnedItems.nodes);
       })
       .catch((error) => {
         console.log("error", error);
@@ -39,5 +52,42 @@ export function Projects() {
     getPinnedRopositories();
   }, []);
 
-  return <PageStructure>{}</PageStructure>;
+  return (
+    <PageStructure>
+      <Box
+        display="grid"
+        flexWrap={"wrap"}
+        gap={10}
+        justifyContent="center"
+        gridTemplateColumns={"1fr 1fr 1fr"}
+      >
+        <Text
+          gridColumn={"1/-1"}
+          justifyContent="center"
+          display={"flex"}
+          fontSize={"30px"}
+          fontWeight={"700"}
+          textTransform={"uppercase"}
+        >
+          {t("projectTitle") as string}
+        </Text>
+
+        {
+          <>
+            {projects.map((project: ProjectTypes, index: any) => (
+              // console.log(project);
+              <ProjectCard
+                key={index}
+                name={project.name}
+                description={project.description}
+                url={project.url}
+                createdAt={project.createdAt}
+                updatedAt={project.updatedAt}
+              />
+            ))}
+          </>
+        }
+      </Box>
+    </PageStructure>
+  );
 }
